f84d290546dcd4c77bdd8e08f7a0f8cdd4719e63
Kevin Tran <ktran050@gmail.com>
2019-11-12 10:31:29 -0800

    first commit

---
+++ focus.js
@@ -0,0 +1,20 @@
+let showFocus = true;
+
+function focusBackground(){
+    console.log(showFocus);
+    if(showFocus){
+        document.getElementById("background").style.zIndex = 10;
+        document.getElementById("focusButton").style.zIndex = 11;
+        document.getElementById("background").style.filter = "blur()";
+        console.log("no blur");
+        showFocus = false;
+    }
+    else{
+        document.getElementById("background").style.zIndex = 10;
+        document.getElementById("focusButton").style.zIndex = 11;
+        document.getElementById("background").style.filter = "blur(2)";
+        console.log("blur");
+        showFocus = true;
+    }
+    return;
+}
